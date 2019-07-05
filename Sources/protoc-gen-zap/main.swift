ddef5b33761b6b1cda1b87f20618abbfb3c0f08a
Otto Suess <ottosuess@protonmail.com>
2019-07-05 17:49:47 +0200

    fix: generated file path

--- Sources/protoc-gen-zap/main.swift
+++ Sources/protoc-gen-zap/main.swift
@@ -8,6 +8,72 @@ func Log(_ message: String) {
 
 var generatedFiles: [String: Int] = [:]
 
+// from apple/swift-protobuf/Sources/protoc-gen-swift/StringUtils.swift
+func splitPath(pathname: String) -> (dir: String, base: String, suffix: String) {
+    var dir = ""
+    var base = ""
+    var suffix = ""
+    #if swift(>=3.2)
+    let pathnameChars = pathname
+    #else
+    let pathnameChars = pathname.characters
+    #endif
+    for c in pathnameChars {
+        if c == "/" {
+            dir += base + suffix + String(c)
+            base = ""
+            suffix = ""
+        } else if c == "." {
+            base += suffix
+            suffix = String(c)
+        } else {
+            suffix += String(c)
+        }
+    }
+    #if swift(>=3.2)
+    let validSuffix = suffix.isEmpty || suffix.first == "."
+    #else
+    let validSuffix = suffix.isEmpty || suffix.characters.first == "."
+    #endif
+    if !validSuffix {
+        base += suffix
+        suffix = ""
+    }
+    return (dir: dir, base: base, suffix: suffix)
+}
+
+enum FileNaming: String {
+    case FullPath
+    case PathToUnderscores
+    case DropPath
+}
+
+func outputFileName(component: String, fileDescriptor: FileDescriptor, fileNamingOption: FileNaming) -> String {
+    let ext = "." + component + ".swift"
+    let pathParts = splitPath(pathname: fileDescriptor.name)
+    switch fileNamingOption {
+    case .FullPath:
+        return pathParts.dir + pathParts.base + ext
+    case .PathToUnderscores:
+        let dirWithUnderscores =
+            pathParts.dir.replacingOccurrences(of: "/", with: "_")
+        return dirWithUnderscores + pathParts.base + ext
+    case .DropPath:
+        return pathParts.base + ext
+    }
+}
+
+func uniqueOutputFileName(component: String, fileDescriptor: FileDescriptor, fileNamingOption: FileNaming) -> String {
+    let defaultName = outputFileName(component: component, fileDescriptor: fileDescriptor, fileNamingOption: fileNamingOption)
+    if let count = generatedFiles[defaultName] {
+        generatedFiles[defaultName] = count + 1
+        return outputFileName(component: "\(count)." + component, fileDescriptor: fileDescriptor, fileNamingOption: fileNamingOption)
+    } else {
+        generatedFiles[defaultName] = 1
+        return defaultName
+    }
+}
+
 func main() throws {
     
     // initialize responses
@@ -23,7 +89,7 @@ func main() throws {
     // process each .proto file separately
     for fileDescriptor in descriptorSet.files {
         if fileDescriptor.services.count > 0 {
-            let grpcFileName = "rpc.zap.swift"
+            let grpcFileName = uniqueOutputFileName(component: "zap", fileDescriptor: fileDescriptor, fileNamingOption: .FullPath)
             let grpcGenerator = Generator(fileDescriptor)
             var grpcFile = Google_Protobuf_Compiler_CodeGeneratorResponse.File()
             grpcFile.name = grpcFileName
